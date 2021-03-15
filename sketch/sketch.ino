177214d5d514e290dfb802d1a8b6e9fdc072da14
Michael Batchelor <mickbatch98@gmail.com>
2021-03-15 23:47:48 +1000

    calling serial commands over bluetooth

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,9 @@
 // #include "MotorDriver.h"
 #include "SerialCommands.h"
 #include "Commands.h"
-
+#include "SoftwareSerial.h"
 #include "avr/io.h"
-
+  
 int someParameter = 0;
 
 void doSomething()
@@ -24,9 +24,12 @@ Commands cmdSet(cmdList, ArraySize(cmdList), varList, ArraySize(varList));
 
 SerialCommands commands(&cmdSet);
 
+SoftwareSerial bt(2, 3);
+
 void setup() {
   Serial.begin(9600);
-
+  bt.begin(9600);
+  
   commands.execute("set someParameter 1");
   cmdSet.set<int>("someParameter", 1);
   cmdSet.call("someCommand");
@@ -34,43 +37,53 @@ void setup() {
   cmdSet.call("someCommand");
 }
 
-void loop() {
-  if (Serial.available()) {
-    int type = -1;
-    switch (commands.write(Serial.read())) {
-    case RT_Get:
-      Serial.print("Value is ");
-      type = commands.returnType();
-      if (type == TypeID<int>()) {
-        Serial.print(commands.returnBuffer<int>());
-      }
-      else if (type == TypeID<float>()) {
-        char buffer[48] = {0};
-        sprintf(buffer, "%f", commands.returnBuffer<float>());
-        Serial.print(buffer);
-      }
-      else if (type == TypeID<bool>()) {
-        Serial.print(commands.returnBuffer<bool>());
-      }
-      else {    
-        Serial.print("Unknown");
-      }
-      Serial.print("\n");
-      break;
-    case RT_Type:
-      Serial.print("Type is ");
-      Serial.print(commands.returnBuffer<int>());
-      Serial.print("\n");
-      break;
-    case RT_Call:
-      Serial.print("Called function\n");
-      break;
-    case RT_Set:
-      Serial.print("Set variable\n");
-      break;
-    case RT_Failure:
-      Serial.print("Bad command\n");
-      break;
+void writeCommand(uint8_t b) {
+  int type = -1;
+  
+  switch (commands.write(b)) {
+  case RT_Get:
+    bt.print("Value is ");
+    type = commands.returnType();
+    if (type == TypeID<int>()) {
+      bt.print(commands.returnBuffer<int>());
     }
+    else if (type == TypeID<float>()) {
+      char buffer[48] = {0};
+      sprintf(buffer, "%f", commands.returnBuffer<float>());
+      bt.print(buffer);
+    }
+    else if (type == TypeID<bool>()) {
+      bt.print(commands.returnBuffer<bool>());
+    }
+    else {    
+      bt.print("Unknown");
+    }
+    bt.print("\n");
+    break;
+  case RT_Type:
+    bt.print("Type is ");
+    bt.print(commands.returnBuffer<int>());
+    bt.print("\n");
+    break;
+  case RT_Call:
+    bt.print("Called function\n");
+    break;
+  case RT_Set:
+    bt.print("Set variable\n");
+    break;
+  case RT_Failure:
+    bt.print("Bad command\n");
+    break;
+  }
+}
+
+void loop() {
+  if (bt.available()) {
+    Serial.write(bt.peek());
+    writeCommand(bt.read());
+  }
+
+  if (Serial.available()) {
+    writeCommand(Serial.read());
   }
 }
