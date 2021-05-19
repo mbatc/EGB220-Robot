d3ada94b7b013f12edbacceeba6d5402b9c6ebbc
Michael Batchelor <mickbatch98@gmail.com>
2021-05-19 22:03:54 +1000

    updated get-requirements.bat

--- get-requirements.bat
+++ get-requirements.bat
@@ -1,6 +1,12 @@
 @echo off
 
-python -m pip install -r remoteroadrunner\requirements.txt
+python -m pip install --upgrade pip
+python -m pip install wheel
+python -m pip install bleak
+python -m pip install asyncio
+python -m pip install imgui[glfw]
+python -m pip install imgui[sdl2]
+python -m pip install pysdl2
 
 copy "remoteroadrunner\SDL2.dll" "C:\windows\system32\SDL2.dll"
 
